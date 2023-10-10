361a87ee4e757e4fc4a9d015294aec73d7ee6b29
Sergey Kruchinkin <kruchinkin.s@yandex.ru>
2023-10-10 11:17:21 +0300

    completed

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<button class="arrow arrow-left">&lt;</button>',
+        nextArrow: '<button class="arrow arrow-right">&gt;</button>'
+    });
+});
